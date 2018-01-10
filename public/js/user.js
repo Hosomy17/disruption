9c05f3c9ec5b8e6534791d3563f475b65725f885
Jackson Daniel <jackson.daniel.17dh@gmail.com>
2018-01-10 19:32:40 -0400

    Adicionado pasta public

---
+++ public/js/user.js
@@ -0,0 +1,15 @@
+var _user = (function ()
+{
+	var name  = 'Player';
+	var hero  = 'steve';
+	var enemyName = 'Enemy';
+	var enemy = 'steve';
+	var turn  = 1;
+
+	return 	{
+				name	: name,
+				turn	: turn,
+				hero	: hero,
+				enemy	: enemy
+			}
+})();
